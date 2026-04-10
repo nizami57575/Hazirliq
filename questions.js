ee6c36bac356f2decda06313c4bd7e3d382f2087
nizami57575 <semkirkonullu235@gmail.com>
2026-04-11 01:05:01 +0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,7 +1,7 @@
 const quizDatabase = [
     {
         week: 1,
-        title: "1-ci Həftə: Giriş Sınağı",
+        title: "1-ci Həftə: Başlanğıc",
         startDate: "2026-04-06",
         questions: [
             { q: "Hansı sözdə ahəng qanunu gözlənilmişdir?", o: ["Kitab", "Qələm", "Təyyarə", "Dünya"], c: "Qələm" },
@@ -13,38 +13,14 @@ const quizDatabase = [
     },
     {
         week: 2,
-        title: "2-ci Həftə: Məntiq Yarışı",
+        title: "2-ci Həftə: Bilik Yarışı",
         startDate: "2026-04-13",
         questions: [
             { q: "Hansı samit cingiltilidir?", o: ["P", "T", "B", "K"], c: "B" },
             { q: "8 × 9 neçə edir?", o: ["64", "72", "81", "56"], c: "72" },
-            { q: "The opposite of 'Hot' is...", o: ["Big", "Cold", "Small", "Fast"], c: "Cold" },
+            { q: "Which one is an animal?", o: ["Chair", "Lion", "Pen", "Table"], c: "Lion" },
             { q: "Как будет 'Школа'?", o: ["Магазин", "Школа", "Сад", "Улица"], c: "Школа" },
             { q: "100 - 64 fərqini tapın:", o: ["36", "46", "34", "44"], c: "36" }
-        ]
-    },
-    {
-        week: 3,
-        title: "3-cü Həftə: Sürət Testi",
-        startDate: "2026-04-20",
-        questions: [
-            { q: "Leksik mənası olmayan sözü seçin:", o: ["Və", "Ev", "Qaçmaq", "Qırmızı"], c: "Və" },
-            { q: "Ən kiçik iki rəqəmli ədəd hansıdır?", o: ["1", "10", "11", "9"], c: "10" },
-            { q: "Where does a fish live?", o: ["Tree", "Forest", "Water", "Sky"], c: "Water" },
-            { q: "Переведи слово 'Яблоко':", o: ["Armud", "Alma", "Nar", "Heyva"], c: "Alma" },
-            { q: "42 ÷ 6 qisməti neçədir?", o: ["6", "7", "8", "9"], c: "7" }
-        ]
-    },
-    {
-        week: 4,
-        title: "4-cü Həftə: Final Mərhələsi",
-        startDate: "2026-04-27",
-        questions: [
-            { q: "Xüsusi isim hansıdır?", o: ["Dağ", "Şəhər", "Gəncə", "Çay"], c: "Gəncə" },
-            { q: "1 metr neçə santimetrdir?", o: ["10 sm", "50 sm", "100 sm", "1000 sm"], c: "100 sm" },
-            { q: "Which day comes after Monday?", o: ["Wednesday", "Friday", "Tuesday", "Sunday"], c: "Tuesday" },
-            { q: "Как сказать 'Привет'?", o: ["Пока", "Привет", "Спасибо", "Пожалуйста"], c: "Привет" },
-            { q: "Hansı cümlə sual cümləsidir?", o: ["Hava istidir", "Dərslərini oxu", "Sən hara gedirsən", "Gəl bura"], c: "Sən hara gedirsən" }
         ]
     }
 ];
