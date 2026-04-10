8bc9e1122a86a0567b68205e70b035f886feb772
nizami57575 <semkirkonullu235@gmail.com>
2026-04-11 00:40:08 +0400

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,16 +1,15 @@
-// HƏFTƏLİK SUAL BAZASI
 const weeklyQuiz = {
     weekTitle: "Aprel Ayı - 2-ci Həftə Sınağı",
     questions: [
         {
-            question: "Hansı söz birləşməsi deyil?",
-            options: ["Dəmir qapı", "Gözəl mənzərə", "Kitab oxumaq", "Gəncə şəhəri"],
-            correct: "Gəncə şəhəri" // Nümunə: Şəhər adları mürəkkəb addır
-        },
-        {
             question: "30-un 20%-i neçə edir?",
             options: ["6", "10", "15", "5"],
             correct: "6"
+        },
+        {
+            question: "Hansı söz birləşməsi deyil?",
+            options: ["Dəmir qapı", "Gözəl mənzərə", "Kitab oxumaq", "Gəncə şəhəri"],
+            correct: "Gəncə şəhəri"
         },
         {
             question: "Which one is a fruit?",
